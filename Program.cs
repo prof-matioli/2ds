4c5102adb0969564e14de41d65fbcedb82bbfab5
Prof. Matioli <matioli@cotil.unicamp.br>
2022-03-14 22:04:53 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosRevisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EXERCÍCIOS DE REVISÃO");
+        }
+    }
+}
